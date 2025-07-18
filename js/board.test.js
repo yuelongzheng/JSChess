1817afe0f2fe648fe55903522a7650d3460302eb
yuelongzheng <132852557+yuelongzheng@users.noreply.github.com>
2025-07-18 11:23:12 +1000

    Testing perft

--- js/board.test.js
+++ js/board.test.js
@@ -14,10 +14,376 @@ describe("Board Properties", () => {
     });
 });
 
-describe("ParseFEN", () => {
+describe("Starting FEN string ParseFEN", () => {
+    board.ParseFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     test('board.enPassant to be 99', () => {
-        board.ParseFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
         expect(board.enPassant).toBe(99);
+    });
+    test('board.side to be COLOURS.WHITE', () => {
+        expect(board.side).toBe(defs.COLOURS.WHITE);
+    })
+    test('board.castlePermission to be 15', () => {
+        expect(board.castlePermission).toBe(15);
+    });
+    test('board.pieceList[21] to be PIECES.wR', () => {
+        expect(board.pieces[21]).toBe(defs.PIECES.wR);
+    });
+    test('board.pieceList[22] to be PIECES.wN', () => {
+        expect(board.pieces[22]).toBe(defs.PIECES.wN);
+    });
+    test('board.pieceList[23] to be PIECES.wB', () => {
+        expect(board.pieces[23]).toBe(defs.PIECES.wB);
+    });
+    test('board.pieceList[24] to be PIECES.wQ', () => {
+        expect(board.pieces[24]).toBe(defs.PIECES.wQ);
+    });
+    test('board.pieceList[25] to be PIECES.wK', () => {
+        expect(board.pieces[25]).toBe(defs.PIECES.wK);
+    });
+    test('board.pieceList[26] to be PIECES.wB', () => {
+        expect(board.pieces[26]).toBe(defs.PIECES.wB);
+    });
+    test('board.pieceList[27] to be PIECES.wN', () => {
+        expect(board.pieces[27]).toBe(defs.PIECES.wN);
+    });
+    test('board.pieceList[28] to be PIECES.wR', () => {
+        expect(board.pieces[28]).toBe(defs.PIECES.wR);
+    });
+    test('board.pieceList[31] to be PIECES.wP', () => {
+        expect(board.pieces[31]).toBe(defs.PIECES.wP);
+    });
+    test('board.pieceList[32] to be PIECES.wP', () => {
+        expect(board.pieces[32]).toBe(defs.PIECES.wP);
+    });
+    test('board.pieceList[33] to be PIECES.wP', () => {
+        expect(board.pieces[33]).toBe(defs.PIECES.wP);
+    });
+    test('board.pieceList[34] to be PIECES.wP', () => {
+        expect(board.pieces[34]).toBe(defs.PIECES.wP);
+    });
+    test('board.pieceList[35] to be PIECES.wP', () => {
+        expect(board.pieces[35]).toBe(defs.PIECES.wP);
+    });
+    test('board.pieceList[36] to be PIECES.wP', () => {
+        expect(board.pieces[36]).toBe(defs.PIECES.wP);
+    });
+    test('board.pieceList[37] to be PIECES.wP', () => {
+        expect(board.pieces[37]).toBe(defs.PIECES.wP);
+    });
+    test('board.pieceList[38] to be PIECES.wP', () => {
+        expect(board.pieces[38]).toBe(defs.PIECES.wP);
+    });
+    test('board.pieceList[41] to be PIECES.EMPTY', () => {
+        expect(board.pieces[41]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[42] to be PIECES.EMPTY', () => {
+        expect(board.pieces[42]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[43] to be PIECES.EMPTY', () => {
+        expect(board.pieces[43]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[44] to be PIECES.EMPTY', () => {
+        expect(board.pieces[44]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[45] to be PIECES.EMPTY', () => {
+        expect(board.pieces[45]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[46] to be PIECES.EMPTY', () => {
+        expect(board.pieces[46]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[47] to be PIECES.EMPTY', () => {
+        expect(board.pieces[47]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[48] to be PIECES.EMPTY', () => {
+        expect(board.pieces[48]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[51] to be PIECES.EMPTY', () => {
+        expect(board.pieces[51]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[52] to be PIECES.EMPTY', () => {
+        expect(board.pieces[52]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[53] to be PIECES.EMPTY', () => {
+        expect(board.pieces[53]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[54] to be PIECES.EMPTY', () => {
+        expect(board.pieces[54]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[55] to be PIECES.EMPTY', () => {
+        expect(board.pieces[55]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[56] to be PIECES.EMPTY', () => {
+        expect(board.pieces[56]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[57] to be PIECES.EMPTY', () => {
+        expect(board.pieces[57]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[58] to be PIECES.EMPTY', () => {
+        expect(board.pieces[58]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[61] to be PIECES.EMPTY', () => {
+        expect(board.pieces[61]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[62] to be PIECES.EMPTY', () => {
+        expect(board.pieces[62]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[63] to be PIECES.EMPTY', () => {
+        expect(board.pieces[63]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[64] to be PIECES.EMPTY', () => {
+        expect(board.pieces[64]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[65] to be PIECES.EMPTY', () => {
+        expect(board.pieces[65]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[66] to be PIECES.EMPTY', () => {
+        expect(board.pieces[66]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[67] to be PIECES.EMPTY', () => {
+        expect(board.pieces[67]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[68] to be PIECES.EMPTY', () => {
+        expect(board.pieces[68]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[71] to be PIECES.EMPTY', () => {
+        expect(board.pieces[71]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[72] to be PIECES.EMPTY', () => {
+        expect(board.pieces[72]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[73] to be PIECES.EMPTY', () => {
+        expect(board.pieces[73]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[74] to be PIECES.EMPTY', () => {
+        expect(board.pieces[74]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[75] to be PIECES.EMPTY', () => {
+        expect(board.pieces[75]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[76] to be PIECES.EMPTY', () => {
+        expect(board.pieces[76]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[77] to be PIECES.EMPTY', () => {
+        expect(board.pieces[77]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[78] to be PIECES.EMPTY', () => {
+        expect(board.pieces[78]).toBe(defs.PIECES.EMPTY);
+    });
+    test('board.pieceList[81] to be PIECES.bP', () => {
+        expect(board.pieces[81]).toBe(defs.PIECES.bP);
+    });
+    test('board.pieceList[82] to be PIECES.bP', () => {
+        expect(board.pieces[82]).toBe(defs.PIECES.bP);
+    });
+    test('board.pieceList[83] to be PIECES.bP', () => {
+        expect(board.pieces[83]).toBe(defs.PIECES.bP);
+    });
+    test('board.pieceList[84] to be PIECES.bP', () => {
+        expect(board.pieces[84]).toBe(defs.PIECES.bP);
+    });
+    test('board.pieceList[85] to be PIECES.bP', () => {
+        expect(board.pieces[85]).toBe(defs.PIECES.bP);
+    });
+    test('board.pieceList[86] to be PIECES.bP', () => {
+        expect(board.pieces[86]).toBe(defs.PIECES.bP);
+    });
+    test('board.pieceList[87] to be PIECES.bP', () => {
+        expect(board.pieces[87]).toBe(defs.PIECES.bP);
+    });
+    test('board.pieceList[88] to be PIECES.bP', () => {
+        expect(board.pieces[88]).toBe(defs.PIECES.bP);
+    });
+    test('board.pieceList[91] to be PIECES.bR', () => {
+        expect(board.pieces[91]).toBe(defs.PIECES.bR);
+    });
+    test('board.pieceList[92] to be PIECES.bN', () => {
+        expect(board.pieces[92]).toBe(defs.PIECES.bN);
+    });
+    test('board.pieceList[93] to be PIECES.bB', () => {
+        expect(board.pieces[93]).toBe(defs.PIECES.bB);
+    });
+    test('board.pieceList[94] to be PIECES.bQ', () => {
+        expect(board.pieces[94]).toBe(defs.PIECES.bQ);
+    });
+    test('board.pieceList[95] to be PIECES.bK', () => {
+        expect(board.pieces[95]).toBe(defs.PIECES.bK);
+    });
+    test('board.pieceList[96] to be PIECES.bB', () => {
+        expect(board.pieces[96]).toBe(defs.PIECES.bB);
+    });
+    test('board.pieceList[97] to be PIECES.bN', () => {
+        expect(board.pieces[97]).toBe(defs.PIECES.bN);
+    });
+    test('board.pieceList[98] to be PIECES.bR', () => {
+        expect(board.pieces[98]).toBe(defs.PIECES.bR);
+    });
+    test('board.pieceList[0] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[0]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[1] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[1]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[2] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[2]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[3] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[3]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[4] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[4]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[5] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[5]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[6] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[6]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[7] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[7]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[8] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[8]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[9] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[9]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[10] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[10]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[11] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[11]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[12] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[12]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[13] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[13]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[14] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[14]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[15] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[15]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[16] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[16]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[17] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[17]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[18] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[18]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[19] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[19]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[20] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[20]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[30] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[30]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[40] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[40]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[50] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[50]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[60] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[60]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[70] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[70]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[80] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[80]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[90] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[90]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[29] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[29]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[39] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[39]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[49] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[49]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[59] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[59]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[69] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[69]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[79] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[79]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[89] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[89]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[99] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[99]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[100] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[100]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[101] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[101]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[102] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[102]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[103] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[103]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[104] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[104]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[105] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[105]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[106] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[106]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[107] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[107]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[108] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[108]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[109] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[109]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[110] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[110]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[111] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[111]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[112] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[112]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[113] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[113]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[114] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[114]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[115] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[115]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[116] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[116]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[117] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[117]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[118] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[118]).toBe(defs.SQUARES.OFFBOARD);
+    });
+    test('board.pieceList[119] to be SQUARES.OFFBOARD', () => {
+        expect(board.pieces[119]).toBe(defs.SQUARES.OFFBOARD);
     });
 });
 
